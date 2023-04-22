77e3af8568c0ebf5ed58fc85032f228c3ec70f7f
EPourmo <ermane.pourmohtasham@gmail.com>
2023-04-22 20:51:44 +0200

    Ajoutez des fichiers projet.

---
+++ BookStore/Models/Domain/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Isbn { get; set; } = null!;
+        [Required]
+        public int TotalPage { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+        [Required]
+        public int PublisherId { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+
+
+    }
+}
---
+++ BookStore/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Genre> Genres => Set<Genre>();
+        public DbSet<Author> Authors => Set<Author>();
+        public DbSet<Publisher> Publishers => Set<Publisher>();
+        public DbSet<Book> Books => Set<Book>();
+
+    }
+}
